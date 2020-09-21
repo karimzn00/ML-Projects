0f2856f7d6b5c73f3d441cc499c875f7077403f5
Karim Zn <62806621+karimzn00@users.noreply.github.com>
2020-09-21 21:33:59 +0100

    Add files via upload

---
+++ net_tools.py
@@ -0,0 +1,41 @@
+import numpy as np
+
+################ ACTIVATION FUNCTIONS ##########################
+
+def sigmoid(inp):
+    if type(inp) in [list, tuple]:
+        inp = np.array(inp)
+    sig = 1.0 / (1 + np.exp(-1 * inp))
+    return sig
+
+def relu(inp):
+    if not(type(inp) in [tuple, list, np.ndarray]):
+        if inp < 0:
+            return 0
+        else:
+            return inp
+    elif type(inp) in [list, tuple]:
+        inp = np.array(inp)
+    relu = inp
+    relu[inp<0] = 0
+    return relu
+
+def tanh(inp):
+    if type(inp) in [list, tuple]:
+        inp = np.array(inp)
+    exn = np.exp(-inp)
+    exp = np.exp(inp)
+    tanh = (exp - exn)/ (exp + exn)
+    return tanh   
+
+def softmax(inp):
+    ex = np.exp(inp)
+    sm = ex / (np.sum(ex))
+    return sm
+
+################################################################
+c = int(input())
+print('relu({}) = {}'.format(c, relu(c)))
+print('sigmoid({}) = {}'.format(c, sigmoid(c)))
+print('tanh({}) = {}'.format(c, tanh(c)))
+print('softmax({}) = {}'.format(c, softmax(c)))
